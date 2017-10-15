cff0d97dd59bc286055ffed0d76d53cd797ce866
Aliaksei Mazurchyk <tortumine@gmail.com>
2017-10-15 23:06:01 +0200

    Major update, need debug offset

--- perfect_maze.asm
+++ perfect_maze.asm
@@ -1,18 +1,20 @@
+.macro OPEN_H_0() {LONG(0xFFFFFFE1)}
+.macro OPEN_H_1() {LONG(0xFFFFE1FF)}
+.macro OPEN_H_2() {LONG(0xFFE1FFFF)}
+.macro OPEN_H_3() {LONG(0xE1FFFFFF)}
+
+.macro OPEN_V_0() {LONG(0xFFFFFF00)}
+.macro OPEN_V_1() {LONG(0xFFFF00FF)}
+.macro OPEN_V_2() {LONG(0xFF00FFFF)}
+.macro OPEN_V_3() {LONG(0x00FFFFFF)}
+
 |; constants
-|; TODO :: fix bug, constant declaration create longs (16b)
-|; but we need words (32b)
+|; TODO :: fix bug, constant declaration create words (16b)
+|; but we need Longs (32b)
 |; TODO :: check lines 94-107,maybe false word offset
-|; but we need words (32b)
+|; but we need Longs (32b)
 H_LINE = 0xFFFFFFFF
 V_LINE = 0xC0C0C0C0
-OPEN_V_0 = 0xFFFFFF00
-OPEN_V_1 = 0xFFFF00FF
-OPEN_V_2 = 0xFF00FFFF
-OPEN_V_3 = 0x00FFFFFF
-OPEN_H_0 = 0xFFFFFFE1
-OPEN_H_1 = 0xFFFFE1FF
-OPEN_H_2 = 0xFFE1FFFF
-OPEN_H_3 = 0xE1FFFFFF
 NB_ROWS = 8
 NB_COLS = 32
 NB_CELLS = 256
@@ -23,6 +25,25 @@ NB_MAZE_WORDS = 512
 CELLS_PER_WORD = 4
 
 TEST_0 = 0xFFE1
+|;TEST_1
+
+OPEN_H0:
+    OPEN_H_0()
+OPEN_H1:
+    OPEN_H_1()
+OPEN_H2:
+    OPEN_H_2()
+OPEN_H3:
+    OPEN_H_3()
+
+OPEN_V0:
+    OPEN_V_0()
+OPEN_V1:
+    OPEN_V_1()
+OPEN_V2:
+    OPEN_V_2()
+OPEN_V3:
+    OPEN_V_3()
 
 |;Functions
 
@@ -73,6 +94,10 @@ RTN()
 |;TMP vars
 |;----------
 |;tmp1 -> R11
+|;tmp2 -> R12
+|;tmp3 -> R13
+|;tmp4 -> R14
+|;tmp5 -> R15
 connect:
 	PUSH(LP)
 	PUSH(BP)
@@ -89,8 +114,7 @@ connect:
 
 	|; TODO :: create functions cf C L54->L59
 	|; row_from_index & col_from_index
-	
-	breakpoint.
+
 	|; R5 = R3/R4 <==> dest_row = dest / nb_cols
 	DIV(R3, R4, R5)	|; RC <- <RA> / <RB>
 	|; R6 = R5*WORDS_PER_ROW <==> row_offset = dest_row*WORDS_PER_ROW
@@ -106,32 +130,116 @@ connect:
 	SUBC(R11, 0x1, R11)
 	AND(R7,R11,R10)
 	
+	MOVE(R9,R15)	|;tmp5 -> R15 == address to edit
+	
+
+	
 	|; vertical connection cf C L61->L75
 	|; [if(dest - source > 1)==>verical] <==> [if(dest - source <= 0)==>verical]
 	SUB(R3, R2, R11)
 	CMPLEC(R11, 0x1, R11)
 	BEQ(R11, horizontal)
-	
-	vertical:	|; open vertical connection
-		ST(R31,0x0044)
-		HALT()
 
+	vertical:	|; open vertical connection
+		ADDC(R15,0x40,R15) |; add a offset vertical cut
+		|; Switch case
+		CMPEQC(R10, 0x0, R11)
+		BT(R11, vertical_0)	|; 0
+		CMPEQC(R10, 0x1, R11)	
+		BT(R11, vertical_1)	|; 0
+		CMPEQC(R10, 0x2, R11)
+		BT(R11, vertical_2)	|; 0
+		CMPEQC(R10, 0x3, R11)	
+		BT(R11, vertical_3)	|; 0
+		vertical_0:				|;case H0
+			LD(R15, 0x60, R11)		|; get table row0
+			CMOVE(OPEN_V0, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x60, R15)		
+			ST(R14, 0x80, R15)		
+			ST(R14, 0xA0, R15)
+			ST(R14, 0xC0, R15)			
+			BEQ(R31, vhend)			|; quit the conditional structure
+			
+		vertical_1:				|;case H1	
+			LD(R15, 0x60, R11)		|; get table row0
+			CMOVE(OPEN_V1, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x60, R15)		
+			ST(R14, 0x80, R15)		
+			ST(R14, 0xA0, R15)
+			ST(R14, 0xC0, R15)		
+			BEQ(R31, vhend)			|; quit the conditional structure
+			
+		vertical_2:				|;case H1	
+			LD(R15, 0x60, R11)		|; get table row0
+			CMOVE(OPEN_V2, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x60, R15)		
+			ST(R14, 0x80, R15)		
+			ST(R14, 0xA0, R15)
+			ST(R14, 0xC0, R15)			
+			BEQ(R31, vhend)			|; quit the conditional structure
+			
+		vertical_3:				|;case H1	
+			LD(R15, 0x60, R11)		|; get table row0
+			CMOVE(OPEN_V3, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x60, R15)		
+			ST(R14, 0x80, R15)		
+			ST(R14, 0xA0, R15)
+			ST(R14, 0xC0, R15)			
+			BEQ(R31, vhend)			|; quit the conditional structure
+
+		
 	horizontal:	|; open horizontal connection
-		MOVE(R1,R15)
-		|; TODO :: "switch-case" on byte_offset (0,1,2 or 3)
-		|; TODO :: adapt H0 case to H1,H2,H3
-		horizontal_0:	|;case H0
-			LD(R15, 0x0, R11)		|; RC <- <<RA>+CC>	(get table row0)
-			ANDC(R11,OPEN_H_0, R12)	|; RC <- <RA> + C	(bit mask and table)
-			ST(R12,0x0,R15)
-			LD(R15, 0x20, R11)		|; RC <- <<RA>+CC>	(get table row1)
-			ANDC(R11,OPEN_H_0, R12)	|; RC <- <RA> + C	(bit mask and table)
-			ST(R12,0x20,R15)
-			HALT()
-		horizontal_1:	|;case H1	
-		horizontal_2:	|;case H2
-		horizontal_3:	|;case H3
+		
+		|; Switch case
+		CMPEQC(R10, 0x0, R11)
+		BT(R11, horizontal_0)	|; 0
+		CMPEQC(R10, 0x1, R11)	
+		BT(R11, horizontal_1)	|; 1
+		CMPEQC(R10, 0x2, R11)	
+		BT(R11, horizontal_2)	|; 2
+		CMPEQC(R10, 0x3, R11)	
+		BT(R11, horizontal_3)	|; 3
 		HALT()
+		horizontal_0:	|;case H0
+			LD(R15, 0x0, R11)		|; get table row0
+			CMOVE(OPEN_H0, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x0, R15)		| <RA>+0x0 <- <RC>	|; H'
+			ST(R14, 0x20, R15)		| <RA>+0x20 <- <RC>	|; H''
+			BEQ(R31, vhend)			|; quit the conditional structure
+		horizontal_1:	|;case H1	
+			LD(R15, 0x0, R11)		|; get table row0
+			CMOVE(OPEN_H1, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x0, R15)		| <RA>+0x0 <- <RC>	|; H'
+			ST(R14, 0x20, R15)		| <RA>+0x20 <- <RC>	|; H''
+			BEQ(R31, vhend)			|; quit the conditional structure
+		horizontal_2:	|;case H2		
+			LD(R15, 0x0, R11)		|; get table row0
+			CMOVE(OPEN_H2, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x0, R15)		| <RA>+0x0 <- <RC>	|; H'
+			ST(R14, 0x20, R15)		| <RA>+0x20 <- <RC>	|; H''
+			BEQ(R31, vhend)			|; quit the conditional structure
+		horizontal_3:	|;case H3
+			LD(R15, 0x0, R11)		|; get table row0
+			CMOVE(OPEN_H3, R12)		|; get bit mask adr
+			LD(R12,0x0,R13)			|; get bit mask
+			AND(R11, R13, R14)		|; 			
+			ST(R14, 0x0, R15)		| <RA>+0x0 <- <RC>	|; H'
+			ST(R14, 0x20, R15)		| <RA>+0x20 <- <RC>	|; H''
+			BEQ(R31, vhend)			|; quit the conditional structure
 	vhend:	|; vertical horizontal end
 	
 	HALT()
@@ -141,9 +249,9 @@ perfect_maze:
 	|; connect test regs
 	|; for horizontal : R3 = R2 +-1
 	|; for vertical : R3 = R2 +-32
-		CMOVE(33,R2) 
-		CMOVE(65,R3)
-		CMOVE(32,R4)
+		CMOVE(6,R2) 
+		CMOVE(7,R3)
+		CMOVE(NB_COLS,R4)
 	
 	PUSH(R4)	|;nb_cols
 	PUSH(R2)	|;source	
