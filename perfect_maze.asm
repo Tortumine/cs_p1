35228479de5becfe28d321f4acc5a2a351ec3707
Aliaksei Mazurchyk <tortumine@gmail.com>
2017-10-24 11:48:11 +0200

    Connect function done

--- perfect_maze.asm
+++ perfect_maze.asm
@@ -1,3 +1,6 @@
+|; TODO connect function push-pop 
+|; TODO :: modify code : literals to regs
+
 .macro OPEN_H_0() {LONG(0xFFFFFFE1)}
 .macro OPEN_H_1() {LONG(0xFFFFE1FF)}
 .macro OPEN_H_2() {LONG(0xFFE1FFFF)}
@@ -9,10 +12,6 @@
 .macro OPEN_V_3() {LONG(0x00FFFFFF)}
 
 |; constants
-|; TODO :: fix bug, constant declaration create words (16b)
-|; but we need Longs (32b)
-|; TODO :: check lines 94-107,maybe false word offset
-|; but we need Longs (32b)
 H_LINE = 0xFFFFFFFF
 V_LINE = 0xC0C0C0C0
 NB_ROWS = 8
@@ -95,6 +94,7 @@ RTN()
 |;tmp3 -> R13
 |;tmp4 -> R14
 |;tmp5 -> R15
+
 connect:
 	PUSH(LP)
 	PUSH(BP)
@@ -109,27 +109,32 @@ connect:
 		MOVE(R11,R2)
 	noswaplabel: 		|; no swap label
 
-	|; TODO :: create functions cf C L54->L59
-	|; row_from_index & col_from_index
-
-	|; R5 = R3/R4 <==> dest_row = dest / nb_cols
-	DIV(R3, R4, R5)	|; RC <- <RA> / <RB>
-	|; R6 = R5*WORDS_PER_ROW <==> row_offset = dest_row*WORDS_PER_ROW
-	MULC(R5, WORDS_PER_ROW, R6)
-	|; modulo : a % b == a & (b - 1)
+	DIVC(R3,NB_COLS,R5) 		|; dest_row = dest / nb_cols
+	MULC(R5,WORDS_PER_ROW,R6) 	|; row_offset = dest_row*WORDS_PER_ROW
 	
-	SUBC(R4, 0x1, R11)
-	AND(R2,R11,R7)
-	DIV(R7, CELLS_PER_WORD, R8)
-	ADD(R6, R8, R9)
-	|; modulo
-	CMOVE(CELLS_PER_WORD,R11) |; need to init a reg for SUBC()
-	SUBC(R11, 0x1, R11)
-	AND(R7,R11,R10)
+	DIVC(R3,NB_COLS,R5) 		|; dest_row = dest / nb_cols
+	MULC(R5,WORDS_PER_ROW,R6) 	|; row_offset = dest_row*WORDS_PER_ROW
 	
-	MOVE(R9,R15)	|;tmp5 -> R15 == address to edit
+	|; source_col = source % nb_cols
+	|; R7 = R2 % nb_cols
+	|; a % n = a - (n * int(a/n)) 
+	|; ==> source % nb_cols = source - (nb_cols * int(source/nb_cols))
+	DIVC(R2,NB_COLS,R7) 		|; ans = source / nb_cols
+	MULC(R7,NB_COLS,R7)			|; ans = ans * nb_cols
+	SUB(R2, R7, R7)				|; source_col = source - ans
 	
-
+	DIVC(R7,CELLS_PER_WORD,R8)	|; word_offset_in_line = source_col / CELLS_PER_WORD
+	ADD(R6, R8, R9)				|; word_offset = row_offset + word_offset_in_line
+	
+	|; byte_offset = source_col % CELLS_PER_WORD
+	|; R10 = R7 % CELLS_PER_WORD
+	DIVC(R7,CELLS_PER_WORD,R10) |; ans = source_col / CELLS_PER_WORD
+	MULC(R10,CELLS_PER_WORD,R10)|; ans = ans * CELLS_PER_WORD
+	SUB(R7, R10, R10)			|; source_col = source_col - ans
+	
+	|; R15 =  WORDS_PER_MEM_LINE * word_offset + source
+	MULC(R9,CELLS_PER_WORD,R15)
+	ADD(R15,R1,R15)
 	
 	|; vertical connection cf C L61->L75
 	|; [if(dest - source > 1)==>verical] <==> [if(dest - source <= 0)==>verical]
@@ -138,9 +143,7 @@ connect:
 	BEQ(R11, horizontal)
 
 	vertical:	|; open vertical connection
-		|; R15 = word_offset + WORDS_PER_MEM_LINE
-		MULC(R9, WORDS_PER_MEM_LINE, R15)		|; RC <- <RA> * <RB>
-		ADDC(R15,0x40,R15) 	|; add a offset vertical cut
+		
 		|; Switch case
 		CMPEQC(R10, 0x0, R11)
 		BT(R11, vertical_0)	|; 0
@@ -241,14 +244,16 @@ connect:
 			BEQ(R31, vhend)			|; quit the conditional structure
 	vhend:	|; vertical horizontal end
 	
-	HALT()
+	DEALLOCATE(20)
+		HALT()
+	RTN()
 	
 	
 perfect_maze:
 	|; for horizontal : R3 = R2 +-1
 	|; for vertical : R3 = R2 +-32
-		CMOVE(4,R2) 
-		CMOVE(5,R3)
+		CMOVE(2,R2) 
+		CMOVE(33,R3)
 		CMOVE(NB_COLS,R4)
 	
 	PUSH(R4)	|;nb_cols
