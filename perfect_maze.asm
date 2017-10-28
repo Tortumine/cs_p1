29ba8d4895201dd2c39b1aff0973e9aa4e7f31eb
vaaty <hansen-jonathan@hotmail.com>
2017-10-28 13:08:22 +0200

    Fix some bug

--- perfect_maze.asm
+++ perfect_maze.asm
@@ -55,15 +55,15 @@
 	|;Ra : Pointers of Array in ram 
 	|;Rb : cell
 	|;return value
-	.macro Get_Cell_Array(Ra, Rb, Rc){
-		|;MULC(Rb, 4, Rc)	|;Multiply cell by for to obtain offset memory
-		ADD(Ra, Rb, Rc)	|;Add offset to pointers of array 
-		LD(Rc, 0, Rc)	|;return value of cell
+	.macro Get_Cell_Array(Ra, Rb, Rc, Rt){
+		MULC(Rb, 4, Rt)	|;Multiply cell by for to obtain offset memory
+		ADD(Ra, Rt, Rt)	|;Add offset to pointers of array 
+		LD(Rt, 0, Rc)	|;return value of cell
 	}
-	.macro Set_Cell_Array(Ra, Rb, Rc){
-		|;MULC(Rb, 4, Rc)	|;Multiply cell by for to obtain offset memory
-		ADD(Ra, Rb, Rc)	|;Add offset to pointers of array 
-		ST(Rc, 0, Rc)	|;save register
+	.macro Set_Cell_Array(Ra, Rb, Rc, Rt){
+		MULC(Rb, 4, Rt)	|;Multiply cell by for to obtain offset memory
+		ADD(Ra, Rt, Rt)	|;Add offset to pointers of array 
+		ST(Rc, 0, Rt)	|;save register
 	}
 	
 |;*****************************************************************************
@@ -312,166 +312,175 @@ connect:
 |;R11	-> tmp
 |;R12	-> tmp
 |;R13	-> tmp
+|;R14	-> tmp
 |;----------
 perfect_maze:
-PUSH(LP)
-PUSH(BP)
-MOVE(SP, BP)
-PUSH(R1)
-PUSH(R2)
-PUSH(R3)
-PUSH(R4)
-PUSH(R5)
-PUSH(R6)
-PUSH(R7)
-PUSH(R8)
-PUSH(R9)
-PUSH(R10)
-PUSH(R11)
-PUSH(R12)
-PUSH(R13)
-LD(BP, -12, R1) |;Get params Maze
-LD(BP, -16, R2) |;Get params Rows
-LD(BP, -20, R3)	|;Get Cols Params
-LD(BP, -24, R4) |;Get Visited Params
-LD(BP, -28, R5) |;Get CurrentCell Params
+	PUSH(LP)
+	PUSH(BP)
+	MOVE(SP, BP)
+	PUSH(R1)
+	PUSH(R2)
+	PUSH(R3)
+	PUSH(R4)
+	PUSH(R5)
+	PUSH(R6)
+	PUSH(R7)
+	PUSH(R8)
+	PUSH(R9)
+	PUSH(R10)
+	PUSH(R11)
+	PUSH(R12)
+	PUSH(R13)
+	PUSH(R14)
+	LD(BP, -12, R1) 		|;Get params Maze
+	LD(BP, -16, R2) 		|;Get params Rows
+	LD(BP, -20, R3)			|;Get Cols Params
+	LD(BP, -24, R4) 		|;Get Visited Params
+	LD(BP, -28, R5) 		|;Get CurrentCell params
+					|; set current cell as visited
+					|; Set mask
+	MODC(R5, 32, R11) 		|;R11 <- current_cell%32
+	CMOVE(1, R12)
+	SHL(R12, R11, R11)		|;R11 <- (1 << Current_cell%32)
+					|;Get VisitedRegister
+	DIVC(R5, 32, R12)		|;R12 <- curr_cell / 32
+	ADD(R4, R12, R12)		|;R12 <- visited[curr_cell / 32]
+	LD(R12, 0, R13)			|;R13 <- &visited[curr_cell / 32]
+	OR(R13, R11, R13)		|;visited[curr_cell /32] |= (1 << curr_cell % 32)
+	ST(R13, 0, R12)			|;Save visited[curr_cell /32] |= (1 << curr_cell % 32)
+					|;neighbours
+					|;How manage that ?
+					|;valid neighbours static array and array size
+	CMOVE(neighbours__, R7)		|;int neighbours[4] 
+	CMOVE(0, R6)			|;n_valid_neighbours = 0; 
 
-|; set current cell as visited
-|; Set mask
-MODC(R5, 32, R11) 	|;R11 <- current_cell%32
-CMOVE(1, R12)
-SHL(R12, R11, R11)	|;R11 <- (1 << Current_cell%32)
-|;Get VisitedRegister
-DIVC(R5, 32, R12)	|;R12 <- curr_cell / 32
-ADD(R4, R12, R12)	|;R12 <- visited[curr_cell / 32]
-LD(R12, 0, R13)		|;R13 <- &visited[curr_cell / 32]
-OR(R13, R11, R13)	|;visited[curr_cell /32] |= (1 << curr_cell % 32)
-ST(R13, 0, R12)		|;Save visited[curr_cell /32] |= (1 << curr_cell % 32)
-|;neighbours
-|;How manage that ?
-|;valid neighbours static array and array size
-CMOVE(neighbours__, R7)	|;int neighbours[4] 
-CMOVE(0, R6)		|;n_valid_neighbours = 0; 
+					|;Check for left neighbour
+					|;int col = col_from_index(curr_cell, nb_cols);
+					|;index % nb_cols;
+	MOD(R5, R3, R10)		|;int col = index % nb_cols
+					|;if (col > 0)
+	CMPLEC(R10, 0, R11)		|;if (col <= 0)
+	BNE(R11, noleft)		|;if R11 != 0 then col <= 0 then col !> 0
+					|;neighbours[n_valid_neighbours++] = curr_cell - 1;
+	Get_Cell_Array(R7, R6, R12, R14)
+	SUBC(R5, 1, R12)		|;loaded Register <- curr_cell -1
+	Set_Cell_Array(R7, R6, R12, R14)|;save register
+	ADDC(R6, 1, R6)			|;n_valid_neighbours++
+	noleft:
+					|;check right neighbour
+					|;(col < nb_cols - 1)
+	SUBC(R3, 1, R11)		|;R11 <- nb_cols - 1
+	CMPLT(R10, R11, R11)		|;R11 <- if(col<nb_cols-1)
+	BEQ(R11, noright)		|;if !(col < nb_cols -1) go to label
+					|;neighbours[n_valid_neighbours++] = curr_cell + 1;
+	Get_Cell_Array(R7, R6, R12, R14)
+	ADDC(R5, 1, R12)		|;loaded Register <- curr_cell + 1
+	Set_Cell_Array(R7, R6, R12, R14)|;save register
+	ADDC(R6, 1, R6)			|;n_valid_neighbours++
+	noright:
+					|;int row = row_from_index(curr_cell, nb_cols);
+	DIV(R5, R3, R10)		|;return index / nb_cols;
+					|;check top neighbour
+					|;if(row > 0) => !(row <= 0)
+	CMPLEC(R10, 0, R11)
+	BNE(R11, notop)
+					|;neighbours[n_valid_neighbours++] = curr_cell - nb_cols;
+	Get_Cell_Array(R7, R6, R12, R14)
+	SUBC(R5, 32, R12)		|;loaded Register <- curr_cell - nb_cols
+	
+	Set_Cell_Array(R7, R6, R12, R14)|;save register
+	ADDC(R6, 1, R6)			|;n_valid_neighbours++
+	notop:
+					|;check bottom neighbour
+					|;if(row < nb_rows - 1)
+	SUBC(R2, 1, R11) 		|;R11 <- nb_rows - 1
+	CMPLT(R10, R11, R11)		|;R11 <- row < nb_rows - 1
+	BEQ(R11, nobottom)
+					|;neighbours[n_valid_neighbours++] = curr_cell + nb_cols;
+	Get_Cell_Array(R7, R6, R12, R14)
+	ADDC(R5, 32, R12)		|;loaded Register <- curr_cell + nb_cols
+	Set_Cell_Array(R7, R6, R12, R14)|;save register
+	ADDC(R6, 1, R6)			|;n_valid_neighbours++
+	nobottom:
+	|;explore valid neighbours
+	while:
+		MOVE(R7, R15)
+		LD(R15, 0, R16)
+		ADDC(R15, 4, R15)
+		LD(R15, 0, R17)
+		ADDC(R15, 4, R15)
+		LD(R15, 0, R18)
+		ADDC(R15, 4, R15)
+		LD(R15, 0, R19)	
+		.breakpoint
 
-|;Check for left neighbour
-|;int col = col_from_index(curr_cell, nb_cols);
-|;index % nb_cols;
-MOD(R5, R3, R10)	|;int col = index % nb_cols
-|;if (col > 0)
-CMPLEC(R10, 0, R11)	|;if (col <= 0)
-BNE(R11, noleft)	|;if R11 != 0 then col <= 0 then col !> 0
-|;neighbours[n_valid_neighbours++] = curr_cell - 1;
-Get_Cell_Array(R7, R6, R12)
-SUBC(R5, 1, R12)	|;loaded Register <- curr_cell -1
-ST(R12, 0, R11)		|;save register
-ADDC(R6, 1, R6)		|;n_valid_neighbours++
-noleft:
-|;check right neighbour
-|;(col < nb_cols - 1)
-SUBC(R3, 1, R11)	|;R11 <- nb_cols - 1
-CMPLT(R10, R11, R11)	|;R11 <- if(col<nb_cols-1)
-BEQ(R11, noright)	|;if !(col < nb_cols -1) go to label
-|;neighbours[n_valid_neighbours++] = curr_cell + 1;
-Get_Cell_Array(R7, R6, R12)
-ADDC(R5, 1, R12)	|;loaded Register <- curr_cell + 1
-ST(R12, 0, R11)		|;save register
-ADDC(R6, 1, R6)		|;n_valid_neighbours++
-noright:
-|;int row = row_from_index(curr_cell, nb_cols);
-DIV(R5, R3, R10)	|;return index / nb_cols;
-|;check top neighbour
-|;if(row > 0) => !(row <= 0)
-CMPLEC(R10, 0, R11)
-BNE(R11, notop)
-|;neighbours[n_valid_neighbours++] = curr_cell - nb_cols;
-Get_Cell_Array(R7, R6, R12)
-SUB(R5, R3, R12)	|;loaded Register <- curr_cell - nb_cols
-ST(R12, 0, R11)		|;save register
-ADDC(R6, 1, R6)		|;n_valid_neighbours++
-notop:
-|;check bottom neighbour
-|;if(row < nb_rows - 1)
-SUBC(R2, 1, R11) 	|;R11 <- nb_rows - 1
-CMPLT(R10, R11, R11)	|;R11 <- row < nb_rows - 1
-BEQ(R11, nobottom)
-|;neighbours[n_valid_neighbours++] = curr_cell + nb_cols;
-Get_Cell_Array(R7, R6, R12)
-ADD(R5, R3, R12)	|;loaded Register <- curr_cell + nb_cols
-ST(R12, 0, R11)		|;save register
-ADDC(R6, 1, R6)		|;n_valid_neighbours++
-nobottom:
+					|;(n_valid_neighbours > 0)
+		CMPLEC(R6, 0, R11)
+		BNE(R11, endwhile)
 
-|;explore valid neighbours
-while:
-|;(n_valid_neighbours > 0)
-CMPLEC(R6, 0, R11)
-BNE(R11, endwhile)
+					|;int random_neigh_index = rand() % n_valid_neighbours;
+		RANDOM()
+		ANDC(R0, 0xFFF, R0)	
+		MOD(R0, R6, R9) 	|;
+					|;int neighbour = neighbours[random_neigh_index];
+		Get_Cell_Array(R7, R9, R8, R14)
+					|;swap(neighbours + n_valid_neighbours - 1, neighbours + random_neigh_index);
+		SUBC(R6, 1, R11)
+		Get_Cell_Array(R7, R11, R12, R14)
+		Get_Cell_Array(R7, R9, R13, R14)
+		Set_Cell_Array(R7, R9, R12, R14)
+		Set_Cell_Array(R7, R11, R13, R14)
+					|;n_valid_neighbours--;		
+		SUBC(R6, 1, R6)
+					|;int visited_bit = (visited[neighbour / 32] >> (neighbour % 32)) & 1; 
+		DIVC(R8, 32, R12)	|;R12 <- neighbours / 32
+		ADD(R4, R12, R12)	|;R12 <- visited[neighbours / 32]
+		LD(R12, 0, R13)		|;R13 <- &visited[neighbours / 32]
+		MODC(R8, 32, R11)	|;(neighbour % 32)
+		SRA(R13, R11, R11)	|;(visited[neighbour / 32] >> (neighbour % 32))
+		ANDC(R11, 1, R11)	|;visited_bit = (visited[neighbour / 32] >> (neighbour % 32)) & 1
 
-|;int random_neigh_index = rand() % n_valid_neighbours;
-RANDOM()
-MOD(R0, R6, R9) 	|;
-|;int neighbour = neighbours[random_neigh_index];
-Get_Cell_Array(R7, R7, R8)
-|;swap(neighbours + n_valid_neighbours - 1, neighbours + random_neigh_index);
-SUBC(R6, 1, R11)
-Get_Cell_Array(R7, R11, R12)
-Get_Cell_Array(R7, R9, R13)
-Set_Cell_Array(R7, R9, R12)
-Set_Cell_Array(R7, R11, R13)
-|;n_valid_neighbours--;
-SUBC(R6, 1, R6)
-|;int visited_bit = (visited[neighbour / 32] >> (neighbour % 32)) & 1; 
-DIVC(R8, 32, R12)	|;R12 <- neighbours / 32
-ADD(R4, R12, R12)	|;R12 <- visited[neighbours / 32]
-LD(R12, 0, R13)		|;R13 <- &visited[neighbours / 32]
-MODC(R8, 32, R11)	|;(neighbour % 32)
-SRA(R13, R11, R11)	|;(visited[neighbour / 32] >> (neighbour % 32))
-ANDC(R11, 1, R11)	|;visited_bit = (visited[neighbour / 32] >> (neighbour % 32)) & 1
-
-|;if (visited_bit == 1)
-|;continue;
-CMPEQC(R11, 1, R11)
-BNE(R11, while)
-|;connect(maze, curr_cell, neighbour, nb_cols);
-
-	|; This is an example of connect function call
-	|; It requires 3 params
-	|; Source cell, Destination cell, Number of Columns
-	|; for horizontal : R3 = R2 +-1
-	|; for vertical : R3 = R2 +-32
-		PUSH(R3)	|;nb_cols
-		PUSH(R5)	|;source	
-		PUSH(R8)	|;dest
-		PUSH(R1)	|;maze
+					|;if (visited_bit == 1)
+					|;continue;
+		CMPEQC(R11, 1, R11)
+		BNE(R11, while)
+					|;connect(maze, curr_cell, neighbour, nb_cols);
+					|; This is an example of connect function call
+					|; It requires 3 params
+					|; Source cell, Destination cell, Number of Columns
+					|; for horizontal : R3 = R2 +-1
+					|; for vertical : R3 = R2 +-32
+		PUSH(R3)		|;nb_cols
+		PUSH(R5)		|;source	
+		PUSH(R8)		|;dest
+		PUSH(R1)		|;maze
 		CALL(connect)
 		DEALLOCATE(4)
-|;perfect_maze(maze, nb_rows, nb_cols, visited, neighbour);
-PUSH(R5) |; CurrentCell
-PUSH(R4) |; Visited
-PUSH(R3) |; Cols
-PUSH(R2) |; Rows
-PUSH(R1) |; Maze
-CALL(perfect_maze)
-DEALLOCATE(5)
-BR(while)
+					|;perfect_maze(maze, nb_rows, nb_cols, visited, neighbour);
+		PUSH(R8) 		|; CurrentCell
+		PUSH(R4) 		|; Visited
+		PUSH(R3) 		|; Cols
+		PUSH(R2) 		|; Rows
+		PUSH(R1) 		|; Maze
+		CALL(perfect_maze)
+		DEALLOCATE(5)
+		BR(while)
 
-endwhile:
-	POP(R13)
-	POP(R12)
-	POP(R11)
-	POP(R10)
-	POP(R9)
-	POP(R8)
-	POP(R7)
-	POP(R6)
-	POP(R5)
-	POP(R4)
-	POP(R3)
-	POP(R2)
-	POP(R1)
-	
-	|;MOVE(R31,r0) |; return 0x0
-	|;MOVE(BP,SP)
-	POP(BP)
-	POP(LP)
-	RTN()
+		endwhile:
+		POP(R14)
+		POP(R13)
+		POP(R12)
+		POP(R11)
+		POP(R10)
+		POP(R9)
+		POP(R8)
+		POP(R7)
+		POP(R6)
+		POP(R5)
+		POP(R4)
+		POP(R3)
+		POP(R2)
+		POP(R1)
+		POP(BP)
+		POP(LP)
+		RTN()
